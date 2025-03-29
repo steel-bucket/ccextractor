b4436fcbb7387d4d32f08639386d5f30d58c3817
Deepnarayan Sett <depnra1@gmail.com>
2025-03-29 23:55:27 +0530

    Hardsubx error fixed

--- src/rust/lib_ccxr/src/activity.rs
+++ src/rust/lib_ccxr/src/activity.rs
@@ -1,35 +1,23 @@
-#![allow(static_mut_refs)] // Temporary fix for mutable static variable
-use crate::common::Options;
 use std::io;
 use std::io::Write;
-use std::os::raw::c_ulong;
+
+use crate::common::Options;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
-pub static NET_ACTIVITY_GUI: AtomicUsize = AtomicUsize::new(0);
+pub static mut NET_ACTIVITY_GUI: AtomicUsize = AtomicUsize::new(0);
 
 pub trait ActivityExt {
     fn activity_report_version(&mut self);
     fn activity_input_file_closed(&mut self);
     fn activity_input_file_open(&mut self, filename: &str);
-    fn activity_report_data_read(&mut self, net_activity_gui: &mut c_ulong);
-    fn activity_video_info(
-        &mut self,
-        hor_size: u32,
-        vert_size: u32,
-        aspect_ratio: &str,
-        framerate: &str,
-    );
-}
-
-pub fn update_net_activity_gui(value: usize) {
-    NET_ACTIVITY_GUI.store(value, Ordering::SeqCst);
+    fn activity_report_data_read(&mut self);
 }
 impl ActivityExt for Options {
     fn activity_report_version(&mut self) {
         if self.gui_mode_reports {
             let mut stderr = io::stderr();
             let version = env!("CARGO_PKG_VERSION");
-            writeln!(stderr, "###VERSION#CCExtractor#{version}").unwrap();
+            writeln!(stderr, "###VERSION#CCExtractor#{}", version).unwrap();
             stderr.flush().unwrap();
         }
     }
@@ -44,34 +32,24 @@ impl ActivityExt for Options {
     fn activity_input_file_open(&mut self, filename: &str) {
         if self.gui_mode_reports {
             let mut stderr = io::stderr();
-            writeln!(stderr, "###INPUTFILEOPEN#{filename}").unwrap();
+            writeln!(stderr, "###INPUTFILEOPEN#{}", filename).unwrap();
             stderr.flush().unwrap();
         }
     }
 
-    fn activity_report_data_read(&mut self, net_activity_gui: &mut c_ulong) {
+    fn activity_report_data_read(&mut self) {
         if self.gui_mode_reports {
             let mut stderr = io::stderr();
-            writeln!(stderr, "###DATAREAD#{}", (*net_activity_gui) / 1000).unwrap();
-            stderr.flush().unwrap();
-        }
-    }
-    fn activity_video_info(
-        &mut self,
-        hor_size: u32,
-        vert_size: u32,
-        aspect_ratio: &str,
-        framerate: &str,
-    ) {
-        if self.gui_mode_reports {
-            let mut stderr = io::stderr();
-            writeln!(
-                stderr,
-                "###VIDEOINFO#{}#{}#{}#{}",
-                hor_size, vert_size, aspect_ratio, framerate
-            )
+            writeln!(stderr, "###DATAREAD#{}", unsafe {
+                NET_ACTIVITY_GUI.load(Ordering::SeqCst) / 1000
+            })
             .unwrap();
             stderr.flush().unwrap();
         }
+    }
+}
+pub fn update_net_activity_gui(value: usize) {
+    unsafe {
+        NET_ACTIVITY_GUI.store(value, Ordering::SeqCst);
     }
 }
