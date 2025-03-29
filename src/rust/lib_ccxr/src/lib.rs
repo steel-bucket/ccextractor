c38458ca7357599e7e44b3ac3d2306a534e19abb
vats004 <=>
2025-03-29 19:14:02 +0530

    some serious code refactoring

--- src/rust/lib_ccxr/src/lib.rs
+++ src/rust/lib_ccxr/src/lib.rs
@@ -1,10 +1,10 @@
 pub mod activity;
 pub mod common;
 pub mod encoder;
+pub mod decoder_xds;
 pub mod hardsubx;
 pub mod net;
 pub mod subtitle;
 pub mod teletext;
 pub mod time;
 pub mod util;
-pub mod decoder_xds;
