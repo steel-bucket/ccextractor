c302d730d1291599f903a4cdba627dbb58c590d1
Deepnarayan Sett <depnra1@gmail.com>
2025-04-16 13:37:36 +0530

    replaced nanomsg with nanomsg_sys

--- src/rust/lib_ccxr/src/lib.rs
+++ src/rust/lib_ccxr/src/lib.rs
@@ -5,3 +5,5 @@ pub mod subtitle;
 pub mod teletext;
 pub mod time;
 pub mod util;
+#[cfg(feature = "enable_sharing")]
+pub mod share;
--- src/rust/src/libccxr_exports/mod.rs
+++ src/rust/src/libccxr_exports/mod.rs
@@ -1,6 +1,8 @@
 //! Provides C-FFI functions that are direct equivalent of functions available in C.
 
 pub mod time;
+#[cfg(feature = "enable_sharing")]
+pub mod share;
 use crate::ccx_options;
 use lib_ccxr::util::log::*;
 use lib_ccxr::util::{bits::*, levenshtein::*};
